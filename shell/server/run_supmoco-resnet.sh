a4f7654f21c2a7b51e4bb5f488d3661f2736f8df
min9kwak <min9kwak@korea.ac.kr>
2022-07-01 18:45:34 -0400

    modify configs for supmoco

--- shell/server/run_supmoco-resnet.sh
+++ shell/server/run_supmoco-resnet.sh
@@ -1,21 +1,22 @@
 echo "Experiments Started"
-SERVER=workstation
-GPUS=0
+SERVER=workstation2
+GPUS=00
 
 DATA_TYPE=pet
-SEGMENT=global
-IMAGE_SIZE=98
+N_SPLITS=10
+N_CV=0
+IMAGE_SIZE=72
 RANDOM_STATE=2021
 
 INTENSITY=scale
 
+EPOCHS=100
 BATCH_SIZE=16
-EPOCHS=200
+OPTIMIZER=adamw
+LEARNING_RATE=0.0001
 
 BACKBONE_TYPE=resnet
 ARCH=50
-
-LEARNING_RATE=0.03
 
 PROJECTOR_DIM=128
 NUM_NEGATIVES=1024
@@ -27,7 +28,7 @@ ALPHAS_DECAY_END="-1,-1"
 
 for ALPHAS in "1.0,1.0" "1.0,0.5", "1.0,2.0"
 do
-  for RANDOM_STATE in 2021
+  for RANDOM_STATE in 2021 2023 2025
 	do
 	  python ./run_supmoco.py \
 		--gpus $GPUS \
@@ -36,27 +37,28 @@ do
 		--root /raidWorkspace/mingu/Data/ADNI \
 		--data_info labels/data_info.csv \
 		--mci_only \
-		--train_size 0.9 \
-		--segment $SEGMENT \
-		--image_size $IMAGE_SIZE \
+		--n_splits $N_SPLITS \
+    --n_cv $N_CV \
+    --image_size $IMAGE_SIZE \
 		--random_state $RANDOM_STATE \
 		--intensity $INTENSITY \
+		--crop \
+		--crop_size 64 \
+		--small_kernel \
 		--flip \
-		--affine \
-		--blur \
-		--blur_std 0.05 \
+		--rotate \
 		--prob 0.5 \
 		--backbone_type $BACKBONE_TYPE \
 		--arch $ARCH \
 		--epochs $EPOCHS \
 		--batch_size $BATCH_SIZE \
-		--optimizer sgd \
+		--optimizer $OPTIMIZER \
 		--learning_rate $LEARNING_RATE \
 		--weight_decay 0.0001 \
 		--cosine_warmup 0 \
 		--cosine_cycles 1 \
 		--cosine_min_lr 0.0 \
-		--save_every 1000 \
+		--save_every 2000 \
 		--enable_wandb \
 		--projector_dim $PROJECTOR_DIM \
 		--num_negatives $NUM_NEGATIVES \
