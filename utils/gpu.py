8b27e6d855cd0f2bf0c949fe4772fc4c2c9899c5
min9kwak <min9kwak@korea.ac.kr>
2023-03-18 10:17:25 -0400

    gpu (workstation2)

--- utils/gpu.py
+++ utils/gpu.py
@@ -1,13 +1,23 @@
 import os
 
-STR2GPU = {'00': 'MIG-4e9bdbba-d0ea-5377-ae8a-a78ccab2f5e5',
-           '01': 'MIG-f45e64c7-dc06-5453-a81e-9bc9ecc30588',
-           '10': 'MIG-91fc8fce-9c3d-57d0-b652-6270d2d1d7d4',
-           '11': 'MIG-5ceca708-e5aa-5675-b039-37a77bd4b6cf',
-           '20': 'MIG-dc45e153-fb1e-5b2d-8a31-d3fb9494cd80',
-           '21': 'MIG-21d343f4-de6e-5d44-9774-e2f3dbab968d',
-           '30': 'MIG-0b2452d4-9b27-530f-a6f1-1c2d05dfaa72',
-           '31': 'MIG-e46a8085-268f-5417-8e5a-a9e20578424d'}
+STR2GPU = {
+    '00': 'MIG-f3304720-4601-5894-bee4-cd0174024e06',
+    '10': 'MIG-35d812cd-2b57-5e55-bb30-890bd9675846',
+    '20': 'MIG-2537493a-e4ca-5b97-850f-4c9e28fa2863',
+    '21': 'MIG-3feaa31a-e279-5752-a99e-573825c15aac',
+    '22': 'MIG-b62af7d4-3a7b-5352-9a6b-e675d109543f',
+    '23': 'MIG-e7905fdd-bb2f-5c66-8b44-0ecca5228ed8',
+    '24': 'MIG-8e436ac2-282e-5f88-810b-a9e2812fa7ba',
+    '25': 'MIG-a4978042-26f3-5231-be6a-e991b7d95832',
+    '26': 'MIG-5e1cfae4-7158-5c8a-ae42-30be508d82b5',
+    '30': 'MIG-cb7e2c1e-63d3-507d-a862-1e574a43ea4f',
+    '31': 'MIG-1c6e06b0-0504-5626-ad12-df578a2e724b',
+    '32': 'MIG-9dc4e79d-857a-53cb-a308-2cf6bdc5ef96',
+    '33': 'MIG-2d7c91c7-e4ee-5584-9c8a-bf6a090d7b96',
+    '34': 'MIG-5c5c61db-3a8e-5e53-809e-d9d06a8e8c73',
+    '35': 'MIG-4baea7de-6e55-5af9-a023-c238d32148b2',
+    '36': 'MIG-7cd7815e-16cd-5648-b3a3-b4f7bd1f1f0a'
+}
 
 
 def set_gpu(config):
